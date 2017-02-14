be004c002f870d51a5e795f84c2678036c1f4e92
annyrtd <fyz95@mail.ru>
2017-02-14 19:59:39 +0300

    reformat code

--- index.js
+++ index.js
@@ -12,23 +12,23 @@ function dashedToCamel(str) {
     });
 }
 
-function parseFile(fileContent, css, varNames) {
+function parseFile(fileContent, css, /*varNames, */js) {
     const {vars: varsToPaste, extractedProperties} = fileContent;
     Object.keys(extractedProperties).forEach(variable => {
         // TODO: create string to paste in js using fixedParts + varsToPaste (name = ${'var' + index} )
         const variableContent = extractedProperties[variable];
-        const resolvedValue = variableContent.resolved;
+        /*const resolvedValue = variableContent.resolved;
         let {fixedParts, vars, values} = parseVariable(variable, resolvedValue);
 
-        vars.forEach((fullValue, index) => {
+        vars.forEach((fullValue) => {
 
             let varName, cssVarName;
             const indexOfComma = fullValue.indexOf(',');
 
             if (indexOfComma == -1) {
-                cssVarName = fullValue.substring(0, fullValue.length - 1).replace(/\s*var\(\s*/, '').trim();
+                cssVarName = fullValue.substring(0, fullValue.length - 1).replace(/\s*var\(\s*!/, '').trim();
             } else {
-                cssVarName = fullValue.substring(0, indexOfComma).replace(/\s*var\(\s*/, '').trim();
+                cssVarName = fullValue.substring(0, indexOfComma).replace(/\s*var\(\s*!/, '').trim();
             }
 
             let item;
@@ -37,10 +37,58 @@ function parseFile(fileContent, css, varNames) {
                 varNames.push({fullValue, varName});
                 item.used = true;
             }
-        });
-
+        });*/
+        js.push(getNewJsToPrint(variableContent, variable, varsToPaste));
         css.push(getNewCssToPrint(variableContent, variable));
     });
+}
+
+function getNewJsToPrint(variableContent, variable, varsToPaste) {
+    let js = '';
+    const resolvedValue = variableContent.resolved;
+    let {fixedParts, vars, values} = parseVariable(variable, resolvedValue);
+    console.log(fixedParts.length, vars.length, values.length)
+    let valueToPrintInJS = fixedParts[0];
+
+    vars.forEach((fullValue, index) => {
+
+        let varName, cssVarName;
+        const indexOfComma = fullValue.indexOf(',');
+
+        if (indexOfComma == -1) {
+            cssVarName = fullValue.substring(0, fullValue.length - 1).replace(/\s*var\(\s*/, '').trim();
+        } else {
+            cssVarName = fullValue.substring(0, indexOfComma).replace(/\s*var\(\s*/, '').trim();
+        }
+
+        let item;
+        if ((item = varsToPaste.find(item => item.name == cssVarName))) {
+            item.used = true;
+            varName = dashedToCamel(cssVarName.replace('--', ''));
+            valueToPrintInJS += `" + ${varName} + \n"`;
+        } else {
+            valueToPrintInJS += escapeForJs(values[index]);
+        }
+
+
+        valueToPrintInJS += fixedParts[index + 1];
+
+    });
+
+    Object.keys(variableContent).filter(property => property != 'resolved' && property != 'associatedVars').forEach(property => {
+        const simpleSelectors = variableContent[property].filter(item => !item.atrules);
+        const selector = simpleSelectors.map(item => item.selectorName).join(',');
+        js += `${escapeForJs(selector)} {${escapeForJs(property)}: ${valueToPrintInJS};}`;
+
+        const selectorsWithAtRules = variableContent[property].filter(item => item.atrules);
+        selectorsWithAtRules.forEach(selector => {
+            selector.atrules.forEach(atrule => {
+                js += `${escapeForJs(atrule)} {${escapeForJs(selector.selectorName)} {${escapeForJs(property)}: ${valueToPrintInJS};}}`;
+            });
+        });
+    });
+
+    return js;
 }
 
 function getNewCssToPrint(variableContent, valueToPrintInCSS) {
@@ -68,10 +116,10 @@ function parseVariable(variable, resolvedValue) {
     let lastIndex = 0;
     const fixedParts = [];
     const vars = [];
-    const varIndentificator = 'var(';
+    const varIdentification = 'var(';
 
-    while (lastIndex < variable.length) {
-        const indexOfVar = variable.indexOf(varIndentificator, lastIndex);
+    while (true) {
+        const indexOfVar = variable.indexOf(varIdentification, lastIndex);
 
         if (indexOfVar === -1) {
             const str = variable.substring(lastIndex);
@@ -81,9 +129,10 @@ function parseVariable(variable, resolvedValue) {
             break;
         }
 
+        //const closingIndex = variable.indexOf(')', indexOfVar);
         let numberOfBrackets = 1;
         let closingIndex;
-        for (closingIndex = indexOfVar + varIndentificator.length; closingIndex < variable.length && numberOfBrackets > 0; closingIndex++) {
+        for (closingIndex = indexOfVar + varIdentification.length; closingIndex < variable.length && numberOfBrackets > 0; closingIndex++) {
             switch (variable[closingIndex]) {
                 case '(':
                     numberOfBrackets++;
@@ -95,8 +144,6 @@ function parseVariable(variable, resolvedValue) {
                     break;
             }
         }
-
-        //const closingIndex = variable.indexOf(')', indexOfVar);
 
         const str = variable.substring(lastIndex, indexOfVar);
         fixedParts.push(escapeForJs(str));
@@ -155,14 +202,15 @@ class ReportalPostCssExtractor {
 
                 const filesToRead = files.filter(item => /^output.*\.json$/.test(item));
 
-                const css = [];
+                const css = [], js = [];
                 const varsToPaste = [];
-                const varNames = [];
+                //const varNames = [];
 
                 filesToRead.forEach(file => {
                     const fileContent = JSON.parse(fs.readFileSync(path.resolve(dirName, file), encoding));
 
-                    parseFile(fileContent, css, varNames);
+                    //parseFile(fileContent, css, varNames);
+                    parseFile(fileContent, css, js);
                     fileContent.vars.filter(item => item.used).forEach(variable => {
                         if (!varsToPaste.find(item => item.name == variable.name)) {
                             varsToPaste.push(variable);
@@ -190,7 +238,8 @@ class ReportalPostCssExtractor {
                     `\t}\n` +
                     `}\n\n\n\n${usualVars.join('\n')}`;
 
-                const jsContent = getJsContent(minimizedCss, varNames);
+                //const jsContent = getJsContent(minimizedCss, varNames);
+                const jsContent = js.join('');
                 const jsText = variablesConfig + '\n\nvar str = "\<style\>";\n\nstr += "' + jsContent + '";\n\nstr += "\<\/style\>";\n\ntext.Output.Append(str);';
 
                 this.saveToAssets(compilation, 'config.js', jsText);
